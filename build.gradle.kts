ba259eab0c7c1429c29b7e1b789297073886efac
Chris Arriola <chrisarriola@google.com>
2021-06-11 10:41:31 -0700

    chore: Adding release workflow.
    Change-Id: I6a5346c007ec1b83235c1a56b32d9c12508e984e

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,39 +12,124 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import com.google.maps.android.rx.androidExtension
 import com.google.maps.android.rx.artifactId
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     }
 }
 
+plugins {
+    `maven-publish`
+    signing
+}
+
+// Shared configs across subprojects
 allprojects {
     group = "com.google.maps.android"
     version = "0.1.0"
 
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
 }
 
+// Publishing and signing info
 subprojects {
-    if (project.artifactId() == null) return@subprojects
+    if (project.artifactId == null) return@subprojects
 
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
+    apply(plugin = "maven-publish")
+    apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "signing")
 
-    // TODO apply shared publishing
-}
+    val sourcesJar = task<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        val libraryExtension = (project.androidExtension as com.android.build.gradle.LibraryExtension)
+        from(libraryExtension.sourceSets["main"].java.srcDirs)
+    }
 
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+    val dokkaHtml = tasks.named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml")
+    val dokkaJavadoc = tasks.named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaJavadoc")
+    val javadocJar = task<Jar>("javadocJar") {
+        dependsOn(dokkaHtml)
+        dependsOn(dokkaJavadoc)
+        archiveClassifier.set("javadoc")
+        from(buildDir.resolve("dokka/javadoc"))
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("aar") {
+                groupId = project.group as String
+                artifactId = project.artifactId
+                version = project.version as String
+
+                pom {
+                    name.set(project.name)
+                    description.set("RxJava bindings for the Maps SDK for Android")
+                    url.set("https://github.com/googlemaps/android-maps-rx")
+
+                    scm {
+                        connection.set("scm:git@github.com:googlemaps/android-maps-rx.git")
+                        developerConnection.set("scm:git@github.com:googlemaps/android-maps-rx.git")
+                        url.set("https://github.com/googlemaps/android-maps-rx")
+                    }
+
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    organization {
+                        name.set("Google Inc.")
+                        url.set("https://developers.google.com/maps")
+                    }
+
+                    developers {
+                        developer {
+                            name.set("Google Inc.")
+                        }
+                    }
+                }
+
+                afterEvaluate {
+                    artifact(buildDir.resolve("outputs/aar/${project.name}-release.aar"))
+
+                    artifact(javadocJar)
+                    artifact(sourcesJar)
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "mavencentral"
+                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = properties["sonatypeUsername"] as String
+                    password = properties["sonatypePassword"] as String
+                }
+            }
+        }
+    }
+
+    // Signing
+    signing {
+        sign(publishing.publications.findByName("aar"))
+    }
 }
