1737a477ff7b42ca01ea69a915b1319fc0cdcd3a
Chris Arriola <chrisarriola@google.com>
2022-03-28 14:37:44 -0700

    feat!: Add default value to PlacesClient extensions (#52)
    BREAKING CHANGE:
    Removes the `actions` parameter from the `findCurrentPlace` extension
    function since no additional configurations can be made to the
    underlying `FindCurrentPlaceRequest` object.
    
    Change-Id: I594bec027cf1ee7f979d926e46fa1387fdf40012

--- places-rx/src/main/java/com/google/maps/android/rx/places/PlacesClientFindCurrentPlaceSingle.kt
+++ places-rx/src/main/java/com/google/maps/android/rx/places/PlacesClientFindCurrentPlaceSingle.kt
@@ -13,29 +13,24 @@ import io.reactivex.rxjava3.core.Single
  * Finds the current place and emits it in a [Single].
  *
  * @param placeFields the fields to return for the retrieved Place
- * @param actions actions to be applied to the [FindCurrentPlaceRequest.Builder]
  * @return the Single emitting the current place
  */
 @RequiresPermission(allOf = ["android.permission.ACCESS_FINE_LOCATION", "android.permission.ACCESS_WIFI_STATE"])
 public fun PlacesClient.findCurrentPlace(
-    placeFields: List<Place.Field>,
-    actions: FindCurrentPlaceRequest.Builder.() -> Unit
+    placeFields: List<Place.Field>
 ): Single<FindCurrentPlaceResponse> =
     PlacesClientFindCurrentPlaceSingle(
         placesClient = this,
         placeFields = placeFields,
-        actions = actions
     )
 
 private class PlacesClientFindCurrentPlaceSingle(
     private val placesClient: PlacesClient,
-    private val placeFields: List<Place.Field>,
-    private val actions: FindCurrentPlaceRequest.Builder.() -> Unit
+    private val placeFields: List<Place.Field>
 ) : MainThreadTaskSingle<FindCurrentPlaceResponse>() {
     @RequiresPermission(allOf = ["android.permission.ACCESS_FINE_LOCATION", "android.permission.ACCESS_WIFI_STATE"])
     override fun invokeRequest(listener: TaskCompletionListener<FindCurrentPlaceResponse>) {
         val request = FindCurrentPlaceRequest.builder(placeFields)
-            .apply(actions)
             .setCancellationToken(listener.cancellationTokenSource.token)
             .build()
         placesClient.findCurrentPlace(request).addOnCompleteListener(listener)
